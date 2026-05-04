3fd94dea0560a46264bc92844448ff26acdb3c81
shenalsanjana <shenal.sanjana.16@gmail.com>
2026-05-04 21:24:16 +0530

    feat(auth): trustHost for netlify proxied requests

--- app/_lib/auth.config.ts
+++ app/_lib/auth.config.ts
@@ -2,6 +2,7 @@
 import type { NextAuthConfig } from "next-auth";
 
 export const authConfig = {
+  trustHost: true,
   session: { strategy: "jwt", maxAge: 60 * 60 * 24 * 30 },
   pages: { signIn: "/login" },
   providers: [],
